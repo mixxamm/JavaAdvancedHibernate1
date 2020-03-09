1f78f826239662a28397eb463f61410eb1cf31e4
mixxamm <maximjanssensj5@gmail.com>
2020-03-09 16:45:58 +0100

    Haal lector data op uit database

--- src/main/java/be/apti/Main.java
+++ src/main/java/be/apti/Main.java
@@ -1,17 +1,27 @@
 package be.apti;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
+
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
-    static final String DB_URL = "jdbc:mysql://localhost:3306";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/toezichten_mc";
     static final String USER = "admin";
     static final String PASS = "admin";
     public static void main(String[] args) throws ClassNotFoundException {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hallo");
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)){
             System.out.println("Verbinding geslaagd");
+            System.out.print("Geef lector naam: ");
+            String lector = scanner.nextLine();
+            System.out.println("Gekozen lector: " + lector);
+            PreparedStatement statement = conn.prepareStatement("SELECT * FROM lecturers WHERE name LIKE ?");
+            statement.setString(1, lector);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            System.out.println("ID: " + resultSet.getString("id") + " Naam: " + resultSet.getString("name"));
         } catch (SQLException e) {
             e.printStackTrace();
         }
