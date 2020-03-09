ae247ec9c91001bf0097a6ebfb00c92594fc976a
mixxamm <maximjanssensj5@gmail.com>
2020-03-09 17:00:46 +0100

    Maak oefening af

--- src/main/java/be/apti/Main.java
+++ src/main/java/be/apti/Main.java
@@ -9,19 +9,26 @@ public class Main {
     static final String DB_URL = "jdbc:mysql://localhost:3306/toezichten_mc";
     static final String USER = "admin";
     static final String PASS = "admin";
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hallo");
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)){
             System.out.println("Verbinding geslaagd");
             System.out.print("Geef lector naam: ");
             String lector = scanner.nextLine();
             System.out.println("Gekozen lector: " + lector);
-            PreparedStatement statement = conn.prepareStatement("SELECT * FROM lecturers WHERE name LIKE ?");
+            PreparedStatement statement = conn.prepareStatement("select * from reservations where lecturer_id like (select id from lecturers where name like ?)");
             statement.setString(1, lector);
             ResultSet resultSet = statement.executeQuery();
-            resultSet.next();
-            System.out.println("ID: " + resultSet.getString("id") + " Naam: " + resultSet.getString("name"));
+            System.out.println("Reservaties voor " + lector);
+            while(resultSet.next()){
+                System.out.println("ID: " + resultSet.getString("id") + " Slot id: " + resultSet.getString("slot_id") + " Gecreëerd op: " + resultSet.getString("created_at") + " Gewijzigd op: " + resultSet.getString("updated_at"));
+                PreparedStatement statement1 = conn.prepareStatement("select * from slots where id = ?");
+                statement1.setString(1, resultSet.getString("slot_id"));
+                ResultSet resultSet1 = statement1.executeQuery();
+                resultSet1.next();
+                System.out.println("Datum: " + resultSet1.getString("date") + " Start: " + resultSet1.getString("start") + " Einde: " + resultSet1.getString("end") + " Duur: " + resultSet1.getString("duration") + " Capaciteit: " + resultSet1.getString("capacity"));
+                System.out.println("---------------------------------------------------------------------------------------------------------\n");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
